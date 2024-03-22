defe726efd0e3a2a3b4dba16fc8706323d726d7e
Akshada Gavhane <62095152+Akshada421@users.noreply.github.com>
2024-03-22 08:06:43 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+//JS Variables & Constants
+var x=10;
+{
+    let x=20;
+    console.log("Value of x: "+x);
+}
+console.log("Value of x: "+x);
+
+/*  OUTPUT:
+    Value of x: 20
+    Value of x: 10
+Let is block scoped and Var can be desclatred and accessed globally
+*/
